524e21128e1ed55cd766029d164aa6aa1b9a2310
hannalee2 <hanna.lee@alleninstitute.org>
2026-01-14 11:33:58 -0800

    Accept cli args (path to image and json file) in main

--- autotomeqc/__main__.py
+++ autotomeqc/__main__.py
@@ -1,32 +1,49 @@
-import logging
 import sys
-import time
-from autotomeqc.config.config_loader import TEST_IMG_DIR
+import logging
+from autotomeqc.config.cli import parse_args, print_arg_info
 from autotomeqc.core.pipeline import AutoTomePipeline
+from autotomeqc.utils.io import save_json_results, save_debug_image
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# Configure logging once here; other modules will inherit it.
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
 
 def main() -> None:
+    # Parse and Validate Arguments
+    args = parse_args()
+    print_arg_info(args)
+    if not args.input_image_path.exists():
+        logger.error(f"Input file not found: {args.input_image_path}")
+        sys.exit(1)
+
+    # Run pipeline
     pipeline = AutoTomePipeline()
     pipeline.start()
 
-    # Temporary: Process all images in the test directory
-    # This simulates feeding images to the pipeline
-    image_files = list(TEST_IMG_DIR.glob("*.jpg"))
-
     try:
-        for file_path in image_files[:2]:
-            logging.info(f"Feeding image: {file_path.name}")
-            pipeline.process_image(file_path)
-            # Simulating capture rate.
-            time.sleep(5)
+        logger.info(f"Processing image: {args.input_image_path.name}")
+        # Returns: (metrics_dict, segmented_image_numpy_array)
+        metrics, segmented_img = pipeline.process_image(args.input_image_path)
+
+        # Save Results to Disk
+        save_json_results(metrics, args.output_path)
+        if args.save_segmented_image:
+            save_debug_image(segmented_img, args.save_segmented_image)
 
     except KeyboardInterrupt:
-        logging.info("Interrupted by user.")
-        
+        logger.info("Interrupted by user.")
+        sys.exit(130)
+
+    except Exception as e:
+        logger.error(f"Critical error: {e}", exc_info=True)
+        sys.exit(1)
+
     finally:
         pipeline.stop()
-        logging.info("Pipeline finished.")
+        logger.info("Pipeline finished.")
 
 if __name__ == "__main__":
     main()
