804b1eeb026a611aba6df03be4aee020fdd8f8f0
hannalee2 <hanna.lee@alleninstitute.org>
2026-01-20 10:33:37 -0800

    Change to src layout from flat layout
    uv is more friendly to src/ layout

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,6 @@ import sys
 import pytest
 from unittest.mock import MagicMock
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 @pytest.fixture
 def mock_config():
     """
