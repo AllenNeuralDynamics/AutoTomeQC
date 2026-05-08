e13b6d0587fe0ae7d86f5885a6ed054ebde887d3
hannalee2 <hanna.lee@alleninstitute.org>
2026-05-08 10:57:57 -0700

    Set container min height size

--- web/src/web/components/main_workspace.py
+++ web/src/web/components/main_workspace.py
@@ -4,9 +4,8 @@ from pathlib import Path
 import numpy as np
 from PIL import Image
 
-
+"""
 def render_main_workspace():
-    """Renders the central image viewing area and returns the image container."""
     ui.query('.q-page').classes('main-workspace bg-grid')
     image_container = ui.column().classes('image-container')
     with image_container:
@@ -16,7 +15,6 @@ def render_main_workspace():
     return image_container
 
 def update_main_workspace(image_container, img_src: Union[str, Path]):
-    """Updates the central image viewing area with a new image."""
     image_container.clear()
     with image_container:
         with ui.element('div').classes('image-wrapper flex justify-center items-center'): # Re-add this wrapper
@@ -26,5 +24,20 @@ def update_main_workspace(image_container, img_src: Union[str, Path]):
             image = Image.fromarray(np.random.randint(0, 255, (100, 100), dtype=np.uint8))
             ui.image(image).classes('w-32')
 
+"""
 
-        
+def render_main_workspace():
+    ui.query('.q-page').classes('flex flex-col items-stretch bg-[#0A0A0A]')
+    image_container = ui.element('div').classes('w-full flex-grow flex items-center justify-center bg-black overflow-hidden min-h-[600px]')
+    with image_container:
+        ui.icon('aspect_ratio', size='6rem').classes('opacity-10 text-white')
+    return image_container
+
+def update_main_workspace(image_container, img_src: Union[str, Path]):
+    image_container.clear()
+    with image_container:
+        #with ui.element('div').classes('flex-1 w-full h-full flex items-center justify-center'):
+        #ui.image(img_src).classes('image-preview').style('w-full h-full object-contain')
+        #image = Image.fromarray(np.random.randint(0, 255, (100, 100), dtype=np.uint8))
+        ui.image(img_src).classes('w-full h-full object-cover')
+    return        
